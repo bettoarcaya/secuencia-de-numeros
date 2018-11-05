2c199652b13b53f3488e0cb4f89924667ab2f36a
bettoarcaya <betoarcaya@gmail.com>
2018-11-05 16:13:19 -0400

    contando la cantidad de ascensos-descensos existen en la coleccion

--- secuencia.py
+++ secuencia.py
@@ -4,7 +4,7 @@ def leerDatos():
     datos = file.read()
     arr = datos.split(" ")
     return arr
-
+    
 def generarSecuencia():
     data = leerDatos()
     inicio = int(data[0])
@@ -13,7 +13,9 @@ def generarSecuencia():
     secuencia.append(inicio)
     i = 0
     par = impar = 0
-    mayor =  cnt = 0
+    mayor =  cnt = counter = 0
+    j = 1
+    aseDes = 1
 
     for i in range(maxIter) :
         if ( secuencia[i] == 1):
@@ -21,13 +23,33 @@ def generarSecuencia():
         if ( secuencia[i] % 2 == 0):
             par = par + 1
             secuencia.append(secuencia[i] / 2)
+            cnt = cnt + 1
         else:
             impar = impar + 1
             secuencia.append( ( secuencia[i] * 3 ) + 1 )
+            cnt = cnt + 1
         
-
-            
-
+        anterior = secuencia[1]
+        while (j != cnt):
+            if ( aseDes == 1):
+                if (secuencia[j] > anterior):
+                    aseDes = 0
+                    counter = counter + 1
+                    mayor = counter if ( counter > mayor ) else mayor
+            else :
+                if ( secuencia[j] < anterior ):
+                    aseDes = 1
+                    counter = counter + 1
+                    mayor = counter if ( counter > mayor ) else mayor
+            j = j + 1
+    
     print secuencia
+    print "S = " + str(inicio)
+    print "L = " + str(maxIter)
+    print "Lof = " + str(cnt)
+    print "P = " + str(par)
+    print "I = " + str(impar)
+    print "N = " + str(mayor / 2)
+    
 
 generarSecuencia()
